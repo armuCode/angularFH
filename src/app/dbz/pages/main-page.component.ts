7f5b8f7e1ca07badb475b24701c235627e0e29fd
armucode <armucode@gmail.com>
2024-04-11 20:54:00 -0500

    Se usan las directivas ngFor ngClass y input and output

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Character } from './interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 6000,
+    },
+    {
+      name: 'Gohan',
+      power: 5000,
+    },
+    {
+      name: 'Piccolo',
+      power: 3000,
+    },
+    {
+      name: 'Freezer',
+      power: 8000,
+    },
+    {
+      name: 'Cell',
+      power: 12000,
+    },
+  ];
+
+  onNewCharacter(character: Character): void {
+    this.characters.push(character);
+  }
+}
